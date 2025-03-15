f0dd02138ca9d10f17d063e65a2f0d7cfad1ded8
Daksh Agrawal <daksha@student.unimelb.edu.au>
2025-03-15 22:10:48 +1100

    load stock data

---
+++ src/app/alphavantage_actions.ts
@@ -0,0 +1,15 @@
+export async function getStockCandles(symbol: string) {
+  const response = await fetch(
+    `https://api.polygon.io/v2/aggs/ticker/${symbol}/range/1/day/2024-03-15/2025-03-15?adjusted=true&sort=asc&limit=50000&apiKey=${process.env.POLYGON_API_KEY}`,
+    { cache: "no-store" }
+  );
+  const data = await response.json();
+  const chartData = data.results.map((candle: any) => {
+    return {
+      date: candle.t,
+      mobile: candle.c,
+      desktop: candle.o,
+    };
+  });
+  return chartData;
+}
